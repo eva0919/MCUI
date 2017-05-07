6e03aeeb44b50bd432ca1f7274e2fbfa0eaa9194
Chen <eva091960601@gmail.com>
2017-05-07 13:42:16 +0800

    Initial Commit

---
+++ MCUI/MCUI.h
@@ -0,0 +1,19 @@
+//
+//  MCUI.h
+//  MCUI
+//
+//  Created by Chen on 2017/5/7.
+//  Copyright © 2017年 Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MCUI.
+FOUNDATION_EXPORT double MCUIVersionNumber;
+
+//! Project version string for MCUI.
+FOUNDATION_EXPORT const unsigned char MCUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MCUI/PublicHeader.h>
+
+
